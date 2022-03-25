1e279621b33329f8037ca5b27dce67d9c0a2005e
Evgeny <evgeny.rfi@gmail.com>
2022-03-25 12:27:41 +0100

    JUnit test success

--- appliSpringJpa/src/test/java/com/m2i/tp/appliSpringJpa/AppliSpringJpaApplicationTests.java
+++ appliSpringJpa/src/test/java/com/m2i/tp/appliSpringJpa/AppliSpringJpaApplicationTests.java
@@ -3,11 +3,47 @@ package com.m2i.tp.appliSpringJpa;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.m2i.tp.appliSpringJpa.dao.DaoEmploye;
+import com.m2i.tp.appliSpringJpa.entity.Employe;
+
 @SpringBootTest
 class AppliSpringJpaApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
+	@Autowired
+	private DaoEmploye daoEmploye;   // l'injection de dependances via le spragwork spring, 
+	//cela initialise la variable DAO employe pourque ça reference un composant pris en charge 
+	//par spring et qui est copatible avec l'interface DaoEmploye dans ce projet seule la classe DaoEMploueJpa correspond à ce critere 
+	
+	
+	
+	
+	
+	
+	@Test // appelé automatiquement après la prise en compte de @Autowired
+ 	public void testAvecSpring() {
+		
+		
+		//Employe nouveauEmploye = new Employe(null, "aurelie", "leboucher", "012345678925", "aurelie@test.com", "login", "pass");
+		
+		//daoEmploye.insertNew(nouveauEmploye);
+		
+		List<Employe> employes = daoEmploye.findAll();
+		for(Employe emp : employes) {
+			System.out.println(emp);
+		}
+		
+		
+	}
 }
+
+	
+
+
